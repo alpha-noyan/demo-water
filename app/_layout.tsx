470a74022f52d1132b9a20e6b9e8fbbbc668d596
noyan-alpha <hammadbaseer4@gmail.com>
2026-05-07 16:37:58 +0500

    set b-i outlet

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,11 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
 
 export default function RootLayout() {
 
   return (
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="bsns-info" />
       </Stack>
   );
 }
